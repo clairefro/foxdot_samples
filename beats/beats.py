d1c570e965caa4b1506ec5f5870a82cbeb4b7cff
clairefro <claire.froelich@gmail.com>
2022-12-26 14:23:40 -0500

    Add beats

--- beats/beats.py
+++ beats/beats.py
@@ -1,5 +1,14 @@
 # basic 4/4
 d1 >> play("x-o-")
 
+# basic 4/4 with hh lead
+d1 >> play("x-o[--]")
+
+# use curlies to introduce variety at random
+d1 >> play("x-o{-o[--]*}")
+
+# swoopy
+d2 >> play("x-o-", rate=[1,2,0.5,-1])
+
 # simple high hat
 d1 >> play("tt(-[---]-)t")
